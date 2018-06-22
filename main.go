a8d3e4f4bb402106bdd744dc0e851e39ac6bf1a7
ifsc01 <ifsclimbing@gmail.com>
2018-06-23 01:43:58 +0800

    fix delete remove service message

--- main.go
+++ main.go
@@ -29,13 +29,34 @@ func main() {
 				log.Println("chat member error", err)
 				return
 			}
-			if err = b.Delete(m); err != nil {
-				log.Println("delete error", err)
-				return
-			}
+
+			b.Delete(m) // delete join service message
 			if err = b.Ban(m.Chat, member); err != nil {
 				log.Println("ban error", err)
 				return
+			}
+
+			// del xx remove xx service message
+			id3Message, err := b.Send(m.Chat, "Member kicked")
+			b.Delete(id3Message)
+			if err != nil {
+				log.Println("send error", err)
+				return
+			}
+			id1 := m.ID
+			id2 := id3Message.ID - 1
+
+			for i := id2; i > id1; i-- {
+				m1, err := b.Send(m.Chat, "reply", &tb.SendOptions{ReplyTo: &tb.Message{ID: i}})
+				if err != nil {
+					log.Println("send reply error", err)
+					return
+				}
+				if m1.ReplyTo.UserLeft.ID == user.ID {
+					b.Delete(m1)         // delete remove service message
+					b.Delete(m1.ReplyTo) // delete reply message
+					return
+				}
 			}
 
 			log.Println("success ban user")
