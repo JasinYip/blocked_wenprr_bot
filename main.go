78698bf782fc02fa75596208cb6f923d0b969833
ifsc01 <ifsclimbing@gmail.com>
2018-06-22 18:12:35 +0800

    init

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	b, err := tb.NewBot(tb.Settings{
+		Token:  os.Getenv("token"),
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle(tb.OnUserJoined, func(m *tb.Message) {
+		user := m.UserJoined
+		if strings.Contains(user.FirstName, "wenprr") || strings.Contains(user.LastName, "wenprr") {
+			member, err := b.ChatMemberOf(m.Chat, user)
+			if err != nil {
+				log.Println("chat member error ", err)
+				return
+			}
+			if err = b.Ban(m.Chat, member); err != nil {
+				log.Println("ban error ", err)
+				return
+			}
+			if err = b.Delete(m); err != nil {
+				log.Println("delete error ", err)
+				return
+			}
+
+			log.Println("success")
+		}
+	})
+	b.Start()
+}
