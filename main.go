592c3595d9bf16f1543479b8875a958bfa23d674
ifsc01 <ifsclimbing@gmail.com>
2018-06-22 22:11:35 +0800

    Delete message first

--- main.go
+++ main.go
@@ -22,18 +22,19 @@ func main() {
 
 	b.Handle(tb.OnUserJoined, func(m *tb.Message) {
 		user := m.UserJoined
+		log.Println("debug: user join", user.FirstName, user.LastName)
 		if strings.Contains(user.FirstName, "wenprr") || strings.Contains(user.LastName, "wenprr") {
 			member, err := b.ChatMemberOf(m.Chat, user)
 			if err != nil {
 				log.Println("chat member error ", err)
 				return
 			}
-			if err = b.Ban(m.Chat, member); err != nil {
-				log.Println("ban error ", err)
-				return
-			}
 			if err = b.Delete(m); err != nil {
 				log.Println("delete error ", err)
+				return
+			}
+			if err = b.Ban(m.Chat, member); err != nil {
+				log.Println("ban error ", err)
 				return
 			}
 
